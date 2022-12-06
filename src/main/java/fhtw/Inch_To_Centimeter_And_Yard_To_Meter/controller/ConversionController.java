0b6ef17674622b61f599ab254d50f7368d54690a
jefferson <ic20b014@technikum-wien.at>
2022-12-06 19:54:26 +0100

    Created the basic controller and initialization

--- src/main/java/fhtw/Inch_To_Centimeter_And_Yard_To_Meter/controller/ConversionController.java
+++ src/main/java/fhtw/Inch_To_Centimeter_And_Yard_To_Meter/controller/ConversionController.java
@@ -1,7 +1,20 @@
 package fhtw.Inch_To_Centimeter_And_Yard_To_Meter.controller;
 
+import fhtw.Inch_To_Centimeter_And_Yard_To_Meter.service.ConversionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConversionController {
+
+    private final ConversionService conversionService;
+
+    public ConversionController(ConversionService conversionService) {
+
+        this.conversionService = conversionService;
+
+    }
+
 }
