b44f3488b7f6630f8addd2326f2067ffdfe51ebf
jefferson <ic20b014@technikum-wien.at>
2022-12-06 20:01:54 +0100

    Created constructors for all classes

--- src/main/java/fhtw/Inch_To_Centimeter_And_Yard_To_Meter/dto/Conversions.java
+++ src/main/java/fhtw/Inch_To_Centimeter_And_Yard_To_Meter/dto/Conversions.java
@@ -1,4 +1,8 @@
 package fhtw.Inch_To_Centimeter_And_Yard_To_Meter.dto;
 
 public class Conversions {
+
+    private float inches = 0;
+    private float yards = 0;
+
 }
--- src/main/java/fhtw/Inch_To_Centimeter_And_Yard_To_Meter/service/ConversionService.java
+++ src/main/java/fhtw/Inch_To_Centimeter_And_Yard_To_Meter/service/ConversionService.java
@@ -1,4 +1,9 @@
 package fhtw.Inch_To_Centimeter_And_Yard_To_Meter.service;
 
+import fhtw.Inch_To_Centimeter_And_Yard_To_Meter.dto.Conversions;
+
 public class ConversionService {
+
+    private final Conversions conversions = new Conversions();
+
 }
